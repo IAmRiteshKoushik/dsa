990af1c80f831f8ee2e80c877d68bcca27966d5b
Ritesh Koushik <riteshkoushik39@gmail.com>
2023-08-01 19:55:34 +0530

    First Java program

---
+++ 00-intro-to-java/01-first-java-program/first-program/Main.java
@@ -0,0 +1,8 @@
+// If the name of the file is Main.java, then the class main is going to be a "public" class. 
+// In Java, everything is a class. There are public classes and private classes.
+
+public class Main{
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
