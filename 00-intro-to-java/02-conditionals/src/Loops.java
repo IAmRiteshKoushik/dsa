81ecf5f4d9f104cbfeee0ec1a7e2bd69d91dd2fa
Ritesh Koushik <riteshkoushik39@gmail.com>
2023-08-02 19:14:29 +0530

    Understood conditionals and looping statements

---
+++ 00-intro-to-java/02-conditionals/src/Loops.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Loops {
+
+    public static void main(String[] args) {
+
+        // For Loop
+        for(int num = 1; num <= 5; num++){
+            System.out.println(num);
+        }
+
+        // Custom for loop
+        // Scanner in = new Scanner(System.in);
+        // int n = in.nextInt();
+
+        // for (int num = 0; num <= n; num++) {
+        //     System.out.print(num + " ");
+        // }
+
+        // While Loop
+        int num = 1;
+        while(num <= 5){
+            System.out.println(num);
+            num++;
+        }
+
+        
+    }
+}
