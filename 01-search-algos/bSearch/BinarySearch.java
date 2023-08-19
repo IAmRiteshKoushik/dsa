e039bc824d421dfefcf7350cad6c54366af56380
Ritesh Koushik <riteshkoushik39@gmail.com>
2023-08-19 19:41:18 +0530

    Add: Binary search

---
+++ 01-search-algos/bSearch/BinarySearch.java
@@ -0,0 +1,33 @@
+/**
+ * BinarySearch
+ */
+public class BinarySearch {
+
+  public static void main(String[] args) {
+    // Sorted array
+    int[] arr = {-18, -12, -4, 0, 2, 3, 4, 15, 16, 18, 22, 45, 89};
+    int target = -12;
+    int ans = binarySearch(arr, target);
+    System.out.println(ans);
+  }
+  static int binarySearch(int[] arr, int target){
+    int start = 0;
+    int end = arr.length - 1;
+
+    while(start <= end){
+      // find the middle element
+      int mid = (start + end) / 2;
+
+      if(target < arr[mid]){
+        end = mid - 1;
+      }else if (target > arr[mid]){
+        start = mid + 1;
+      } else {
+        // ans found
+        return mid;
+      }
+    } 
+    // if element does not exist
+    return -1;
+  }
+}
