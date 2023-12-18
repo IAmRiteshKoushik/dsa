a4f0c56c569dd7e2e9b4efe1f9947c90b613e2e5
Ritesh Koushik <riteshkoushik39@gmail.com>
2023-12-18 21:27:33 +0530

    add: Access control

---
+++ 05-oop/accessControl/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+  public static void main(String[] args) {
+    A obj = new A(10, "Kunal");
+
+    // objectives
+    // 1. access the data members
+    // 2. modify the data members
+  } 
+}
