eeeab5fd21a270348ff9c3f1a47bd5b2f8c37459
IAmRiteshKoushik <riteshkoushik39@gmail.com>
2024-06-13 15:09:34 +0530

    (easy) Binary Tree PostOrder traversal

---
+++ qs/p145-binTree-postorder-traversal/Solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        traverse(root, result);
+        return result;
+    }
+    private void traverse(TreeNode root, List<Integer> result){
+        if (root == null){
+            return;
+        }
+        traverse(root.left, result);
+        traverse(root.right, result);
+        result.add(root.val);
+    }
+}
+
